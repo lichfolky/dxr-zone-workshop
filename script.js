065214aa095e2782dfe426ea58420b1caaed5e8c
Lichfolky <lichfolky@gmail.com>
2022-11-09 14:04:38 +0100

    Infinite images

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ canvas.height = window.innerHeight;
 
 const sizeX = 150;
 const sizeY = 150;
-let imageOdd = new Image();
-imageOdd.src = "https://source.unsplash.com/random/150x150";
-let imageEven = new Image();
-imageEven.src = "https://source.unsplash.com/random/149x149";
-let image = imageOdd;
-let odd = false;
+let rngNum = 1;
+
+let image = new Image();
+image.src = `https://source.unsplash.com/random/${sizeX}x${sizeY}/?sig=${rngNum}`;
 
 const ctx = canvas.getContext("2d");
 
@@ -22,15 +20,12 @@ const handleMouseMove = (event) => {
   }
 };
 
-const handleMouseClick = (event) => {
-  if (odd) {
-    image = imageOdd;
-  } else {
-    image = imageEven;
-  }
-  odd = !odd;
-  ctx.drawImage(image, event.pageX - sizeX / 2, event.pageY - sizeY / 2, sizeX, sizeY);
-  writeText();
+async function handleMouseClick(event) {
+  let newImage = new Image();
+  rngNum = getRandomInt(2, 1000);
+  newImage.src = `https://source.unsplash.com/random/${sizeX}x${sizeY}/?sig=${rngNum}`;
+  await newImage.decode();
+  image = newImage;
 };
 
 const writeText = () => {
